cc15e0d3b23097dc7e2389ac0ab199850f8ace7d
abulisu12 <abulfazlsoultnii12@gmail.com>
2023-05-27 10:26:20 +0330

    editing

--- Script/Main.js
+++ Script/Main.js
@@ -3,16 +3,13 @@ let clear = document.getElementById('clear')
 let ac = document.getElementById('ac')
 let equal = document.getElementById('equal')
 let answer = document.getElementById('answer')
-let getLength = answer.value.length
 let clickButton = (event) => {
     let value = event.target.dataset.num
     answer.value += value
 }
 buttons.forEach(button => button.addEventListener('click', clickButton))
 let doClearDisplay = () => answer.value = '';
-let resetToZero = () => {
-    answer.value = 0
-}
+let resetToZero = () => answer.value = 0;
 let getAnswer = () => answer.value = eval(answer.value);
 ac.addEventListener('click', doClearDisplay)
 clear.addEventListener('click', resetToZero)
